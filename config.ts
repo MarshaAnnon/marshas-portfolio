00d9318b7acb17e4f0699f3ae8ed759561cce665
marsha <marshaannon@gmail.com>
2020-11-15 21:51:54 -0500

    add description and about

--- config.ts
+++ config.ts
@@ -9,9 +9,9 @@ const siteMetadata = {
     twoColumnWall: true,
     cookiePolicy: true,
     introTag: `Web developer/Software Engineer`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet accumsan arcu. Proin ac consequat arcu.`,
+    description: `Intuitive Communicator, INTP | Full Stack / Front End Developer`,
     about:
-        "Cras accumsan a lectus at tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus elementum dapibus dictum. Mauris auctor quam nec est tincidunt hendrerit. Donec pulvinar lobortis mauris. Cras vulputate ullamcorper ligula a rhoncus. Nunc venenatis elementum ligula in semper. Mauris malesuada purus nunc, et ultricies leo aliquam ac. Ut sit amet nunc id magna accumsan hendrerit in eget metus.",
+        "Software engineer with an understanding of Front-end and Back-end languages. I merge my passion for user experience with my technical knowledge to produce unique digital experiences. I am at my best when finding creative solutions to difficult problems. I believe my background in education and entrepreneurship will assist in adding value to any team I join.",
     author: `@_akzhy`,
     blogItemsPerPage: 10,
     portfolioItemsPerPage: 10,
